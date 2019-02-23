5296d6dd1e23b75406748eafdab2bfdfb17b439e
RaiderSoap <info@chivalrystudio.com>
2019-02-23 10:29:59 -0800

    small refactor

--- index.js
+++ index.js
@@ -8,23 +8,20 @@ init();
 
 /**
  * @param object {THREE.Object3D}
- * @constructor
  */
 function OnObjLoad(object) {
   let mesh = object.children[0];
   mesh.material = new THREE.MeshNormalMaterial();
+  mesh.material.flatShading = true;
   scene.add(object);
 
   let geo = mesh.geometry;
 
   let vnh = new THREE.VertexNormalsHelper(mesh, 0.05);
-  scene.add(vnh);
+  // scene.add(vnh);
 
   let box = new THREE.BoxHelper(object, 0xffff00);
   scene.add(box);
-
-  console.log(geo);
-  console.log(geo.attributes.position);
 
   render();
 }
@@ -36,41 +33,26 @@ function initGUI() {
 function init() {
   initGUI();
 
-  // Scene
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x999999);
-  scene.add(new THREE.AmbientLight(0x999999));
-
-  // Camera
-  camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 500);
-
-  // camera.up.set(0, 0, 1); // Z is up for objects intended to be 3D printed.
-  camera.position.set(3, 3, -8);
-  camera.add(new THREE.PointLight(0xffffff, 0.8));
-  scene.add(camera);
-
-  // Grid Helper
-  const grid = new THREE.GridHelper(10, 10, 0xffffff, 0x555555);
-  // grid.rotateOnAxis(new THREE.Vector3(1, 0, 0), 90 * (Math.PI / 180));
-  scene.add(grid);
-
   // Renderer
   renderer = new THREE.WebGLRenderer({antialias: true});
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  // OBJ
-  loader.load('./models/1abeca7159db7ed9f200a72c9245aee7.obj', OnObjLoad);
+  // Scene
+  scene = new AppScene();
 
-  // Control
-  const controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.addEventListener('change', render);
-  controls.target.set(0, 0, 0);
-  controls.update();
+  // OBJ
+  loadModel('./models/1b5b5a43e0281030b96212c8f6cd06e.obj');
 
   window.addEventListener('resize', onWindowResize, false);
   document.addEventListener('mousemove', onDocumentMouseMove, false);
+}
+
+function loadModel(path, position) {
+  let model = loader.load(path, OnObjLoad);
+
+  console.log(model);
 }
 
 function render() {
---
+++ js/app_scene.js
@@ -0,0 +1,24 @@
+class AppScene extends THREE.Scene {
+
+  constructor(...args) {
+    super(...args);
+
+    this.background = new THREE.Color(0x999999);
+
+    camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(3, 3, -8);
+    camera.add(new THREE.PointLight(0xffffff, 0.8));
+
+    const light = new THREE.AmbientLight(0x999999);
+    const grid = new THREE.GridHelper(10, 10, 0xffffff, 0x555555);
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    controls.addEventListener('change', render);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    this.add(light);
+    this.add(camera);
+    this.add(grid);
+  }
+}
