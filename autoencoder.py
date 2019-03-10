32d4b276bb5950942c3a1b22942c3b1b8423cb0f
Seiji Emery <seijiemery@gmail.com>
2019-03-09 23:28:28 -0800

    added CLI

--- autoencoder.py
+++ autoencoder.py
@@ -287,7 +287,7 @@ class ArgumentParsingException (Exception):
         super().__init__(*args, **kwargs)
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='Naval Fate 2.0')
+    args = docopt(__doc__)
     # print(args)
 
     """ Validate arguments """
